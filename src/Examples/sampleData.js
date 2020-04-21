47b6f3aae233a803e3182ad2889c2677bf12e136
sarifulin <khlopilia@gmail.com>
2020-04-21 02:38:10 +0200

    tech task -added more data -formatted data

--- src/Examples/sampleData.js
+++ src/Examples/sampleData.js
@@ -8,7 +8,7 @@ export const list = [
   "Kurtis Weissnat",
   "Nicholas Runolfsdottir V",
   "Glenna Reichert",
-  "Clementina DuBuque"
+  "Clementina DuBuque",
 ];
 
 export const columnConfig = [
@@ -16,7 +16,7 @@ export const columnConfig = [
   { key: "username", name: "Nickname", width: 100 },
   { key: "email", name: "Private E-mail", width: 180 },
   { key: "phone", name: "Phone", width: 150 },
-  { key: "website", name: "Website Please Visit", width: 100 }
+  { key: "website", name: "Website Please Visit", width: 100 },
 ];
 export const data = [
   {
@@ -24,30 +24,29 @@ export const data = [
     username: "Bret",
     email: "Sincere@april.biz",
     phone: "1-770-736-8031 x56442",
-    website: "hildegard.org"
+    website: "hildegard.org",
   },
   {
     name: "Mrs. Dennis Schulist",
     username: "Leopoldo",
     email: "Karley_Dach@jasper.info",
     phone: "1-477-935-8478 x6430",
-    website: "ola.org"
+    website: "ola.org",
   },
   {
     name: "Kurtis Weissnat",
     username: "Elwyn.Skiles",
     email: "Telly.Hoeger@billy.biz",
     phone: "210.067.6132",
-    website: "elvis.io"
+    website: "elvis.io",
   },
   {
     name: "Nicholas Runolfsdottir V",
     username: "Maxime_Nienow",
     email: "Sherwood@rosamond.me",
     phone: "586.493.6943 x140",
-    website: "jacynthe.com"
+    website: "jacynthe.com",
   },
-  
 ];
 export const data2 = [
   {
@@ -55,41 +54,133 @@ export const data2 = [
     username: "Antonette",
     email: "Shanna@melissa.tv",
     phone: "010-692-6593 x09125",
-    website: "anastasia.net"
+    website: "anastasia.net",
   },
   {
     name: "Clementine Bauch",
     username: "Samantha",
     email: "Nathan@yesenia.net",
     phone: "1-463-123-4447",
-    website: "ramiro.info"
+    website: "ramiro.info",
   },
   {
     name: "Patricia Lebsack",
     username: "Karianne",
     email: "Jul.OConner@kory.org",
     phone: "493-170-9623 x156",
-    website: "kale.biz"
+    website: "kale.biz",
   },
   {
     name: "Chelsey Dietrich",
     username: "Kamren",
     email: "Lucio_Hettinger@annie.ca",
     phone: "(254)954-1289",
-    website: "demarco.info"
+    website: "demarco.info",
   },
   {
     name: "Glenna Reichert",
     username: "Delphine",
     email: "Chaimott@dana.io",
     phone: "(775)976-6794 x41206",
-    website: "conrad.com"
+    website: "conrad.com",
   },
   {
     name: "Clementina DuBuque",
     username: "Moriah.Stanton",
     email: "Rey.Padberg@karina.biz",
     phone: "024-648-3804",
-    website: "ambrose.net"
-  }
+    website: "ambrose.net",
+  },
+];
+
+export const columnConfig_t1_1 = [
+  { key: "name", name: "Name", width: 120 },
+  { key: "username", name: "Nickname", width: 100 },
+];
+
+export const data_t1_1 = [
+  {
+    name: "Leanne Graham",
+    username: "Bret",
+  },
+  {
+    name: "Mrs. Dennis Schulist",
+    username: "Leopoldo",
+  },
+  {
+    name: "Kurtis Weissnat",
+    username: "Elwyn.Skiles",
+  },
+  {
+    name: "Nicholas Runolfsdottir V",
+    username: "Maxime_Nienow",
+  },
+];
+
+export const columnConfig_t1_2 = [
+  { key: "name", name: "Name", width: 120 },
+  { key: "email", name: "Private E-mail", width: 180 },
+  { key: "phone", name: "Phone", width: 150 },
+];
+
+export const data_t1_2 = [
+  {
+    name: "Leanne Graham",
+    email: "Sincere@april.biz",
+    phone: "1-770-736-8031 x56442",
+  },
+  {
+    name: "Mrs. Dennis Schulist",
+    email: "Karley_Dach@jasper.info",
+    phone: "1-477-935-8478 x6430",
+  },
+  {
+    name: "Nicholas Runolfsdottir V",
+    email: "Sherwood@rosamond.me",
+    phone: "586.493.6943 x140",
+  },
+];
+
+export const columnConfig_t1_3 = [
+  { key: "username", name: "Nickname", width: 100 },
+  { key: "website", name: "Website Please Visit", width: 100 },
+  { key: "visitors", name: "Site's Visitors", width: 50 },
+];
+
+export const data_t1_3 = [
+  {
+    username: "Leopoldo",
+    website: "ola.org",
+    visitors: "1024",
+  },
+  {
+    username: "Elwyn.Skiles",
+    website: "elvis.io",
+    visitors: "32",
+  },
+  {
+    username: "Maxime_Nienow",
+    website: "jacynthe.com",
+    visitors: "15000",
+  },
+];
+
+export const columnConfig_t2_1 = [
+  { key: "name", name: "Name", width: 100 },
+  { key: "date", name: "Date", width: 100 },
+];
+
+export const data_t2_1 = [
+  {
+    name: "Leanne Graham",
+    date: "1953-04-15",
+  },
+  {
+    name: "Mrs. Dennis Schulist",
+    date: "1963-10-10",
+  },
+  {
+    name: "Nicholas Runolfsdottir V",
+    date: "2020-04-15",
+  },
 ];
