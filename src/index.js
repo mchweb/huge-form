1dc0938aa875104585b47f35e62562aa61b5f2de
sarifulin <khlopilia@gmail.com>
2020-04-21 23:43:09 +0200

    removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import ComponentsShowcase from "./Examples/ComponentsShowcase";
 import SimpleFormExample from "./Examples/SimpleFormExample";
 import GroupExample from "./Examples/GroupExample";
 import StyledFormExample from "./Examples/StyledFormExample";
-import GroupOne from "./Tasks/GroupOne";
-import GroupTwo from "./Tasks/GroupTwo";
-import GroupThree from "./Tasks/GroupThree";
+import IliaKhTask from "./Tasks/IliaKhTask";
 
 
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<GroupTwo />, document.getElementById("root"));
+ReactDOM.render(<IliaKhTask />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
